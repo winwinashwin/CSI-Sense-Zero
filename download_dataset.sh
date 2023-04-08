c96d262b03528c536368db9bc65be120cf551a4f
Ashwin A Nayar <ashwin5059198@gmail.com>
2023-04-08 13:49:05 +0530

    Update dataset

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-wget https://github.com/winwinashwin/CSI-Sense-Zero/releases/download/dataset-release-tag/csi-sense-dataset.tar.gz
-wget https://github.com/winwinashwin/CSI-Sense-Zero/releases/download/dataset-release-tag/csi-sense-dataset.tar.gz.sha256sum
-sha256sum -c csi-sense-dataset.tar.gz.sha256sum
+wget https://github.com/winwinashwin/CSI-Sense-Zero/releases/download/dataset-release-tag/dataset.tar.gz
+wget https://github.com/winwinashwin/CSI-Sense-Zero/releases/download/dataset-release-tag/dataset.tar.gz.sha256sum
+sha256sum -c dataset.tar.gz.sha256sum
 
-tar -xvzf csi-sense-dataset.tar.gz
+tar -xvzf dataset.tar.gz
